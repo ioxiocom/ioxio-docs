39c00efe87a773ef3a5d164c2dd617639edd1113
Solo <58733599+omarudolley@users.noreply.github.com>
2024-04-25 13:37:57 +0300

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,153 @@
-<script>
-  import Title from "$lib/Title.svelte"
+<script xmlns="http://www.w3.org/1999/html">
   import A from "$lib/A.svelte"
-  import Breadcrumbs from "$lib/Breadcrumbs.svelte"
+  import DataspaceIcon from "$lib/images/dataspace-icon.svg?dataurl"
+  import DataSchemasIcon from "$lib/images/schemas-secondary-icon.svg?dataurl"
+  import QRCodeIcon from "$lib/images/qr-code-secondary-icon.svg?dataurl"
+  import ArrowIcon from "$lib/images/link-arrow-icon.svg"
 
-  /** @type {import('./$types').PageData} */
-  export let data
+  const cards = [
+    {
+      title: "IOXIO Tags™",
+      href: "/tags/",
+      alt: "QR code",
+      img: QRCodeIcon,
+    },
+    {
+      title: "Schemas for data objects",
+      href: "/schemas/",
+      alt: "Data schemas",
+      img: DataSchemasIcon,
+    },
+  ]
 </script>
 
-<Title title={`IOXIO®️ Documentation portal`} />
-
-<Breadcrumbs path={data.path} route={data.route} />
-
-<p>
-  Documentation for various <A href="https://ioxio.com">IOXIO®</A> technologies.
-</p>
-
+<div class="wrapper">
+  <div class="content">
+    <h1 class="title">Welcome to IOXIO® Documentation portal</h1>
+    <p class="description">Documentation for various IOXIO® technologies.</p>
+    <p class="description">
+      Visit <A href="https://ioxio.com">IOXIO®</A> to learn more about IOXIO®
+    </p>
+  </div>
+  <img src={DataspaceIcon} alt="Dataspace" />
+</div>
 <div class="cards">
-  <a href="/tags/">
-    <div class="card">IOXIO Tags™</div>
-  </a>
-  <a href="/schemas/">
-    <div class="card">Schemas for data objects</div>
-  </a>
+  {#each cards as card}
+    <div class="card">
+      <h2>{card.title}</h2>
+      <img src={card.img} alt={card.alt} />
+      <a class="link" href={card.href}
+        ><span>Read more </span>
+        <ArrowIcon />
+      </a>
+    </div>
+  {/each}
+  <!-- Reserved -->
+  <div class="reserved" />
 </div>
 
 <style lang="scss">
+  @import "$styles/setup";
   .cards {
     display: flex;
     flex-direction: row;
-    margin: 4rem 0;
+    margin: $spacing-03 0;
+    gap: $spacing-03;
 
-    align-items: stretch;
-    gap: 3rem;
+    @include tablet-and-below() {
+      padding: 0 $spacing-02;
+    }
+    @include mobile() {
+      flex-direction: column;
+    }
   }
 
-  a {
-    display: flex;
-    flex-direction: column;
+  .reserved {
+    flex: 1 0 0;
+    @include tablet-and-below() {
+      display: none;
+    }
   }
 
   .card {
-    flex-grow: 1;
-
+    padding: $spacing-02;
     display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
+    flex-direction: column;
+    background: #192935;
+    justify-content: space-between;
+    border-radius: 10px;
+    gap: $spacing-01;
+    position: relative;
+    height: 10rem;
+    flex: 1 0 0;
 
-    width: 8rem;
-    border: 1px solid #0f2543;
-    background: #cbd5e2;
-    color: #000;
-    border-radius: 8px;
-    padding: 2.5rem;
+    @include mobile() {
+      flex-basis: auto;
+    }
+
+    .link {
+      display: flex;
+      flex-direction: row;
+      width: max-content;
+      color: $color-success-main;
+      font-weight: 600;
+      line-height: 150%;
+      text-decoration: none;
+      align-items: center;
+      gap: $spacing-01;
+      z-index: 99;
+    }
+
+    img {
+      position: absolute;
+      width: 6.125rem;
+      height: 6.125rem;
+      right: $spacing-02;
+      bottom: $spacing-02;
+    }
+
+    h2 {
+      font-size: $h2-font-size;
+      font-weight: 600;
+      line-height: 150%;
+      z-index: 99;
+    }
+  }
+
+  .wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 $spacing-03;
+
+    @include tablet-and-below() {
+      padding: $spacing-03 $spacing-02;
+    }
+
+    @include mobile() {
+      flex-direction: column;
+    }
+
+    .content {
+      display: flex;
+      flex-direction: column;
+      column-gap: 1rem;
+
+      .title {
+        font-size: $h1-font-size;
+        font-weight: 600;
+        line-height: 150%;
+      }
+
+      .description {
+        font-size: $text-body-font-size;
+        line-height: 150%;
+      }
+    }
+  }
+
+  img {
+    width: 42.75rem;
+    margin: 0 0 0 auto;
   }
 </style>
