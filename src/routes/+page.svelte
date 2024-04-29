c90c8b6489755e6990736dd6f9aca9d48228eaab
Solo <58733599+omarudolley@users.noreply.github.com>
2024-04-29 07:54:36 +0400

    renaming svgs and clean ups

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script xmlns="http://www.w3.org/1999/html">
   import A from "$lib/A.svelte"
-  import DataspaceIcon from "$lib/images/dataspace-icon.svg?dataurl"
-  import DataSchemasIcon from "$lib/images/schemas-secondary-icon.svg?dataurl"
-  import QRCodeIcon from "$lib/images/qr-code-secondary-icon.svg?dataurl"
+  import FrontpageHeroImage from "$lib/images/frontpage-hero-image.svg?dataurl"
+  import DataSchemasIcon from "$lib/images/card-schemas-icon.svg?dataurl"
+  import QRCodeIcon from "$lib/images/card-qr-code-icon.svg?dataurl"
   import ArrowIcon from "$lib/images/link-arrow-icon.svg"
+  import Grid from "$lib/components/Grid.svelte"
 
   const cards = [
     {
@@ -22,132 +23,121 @@
 </script>
 
 <div class="wrapper">
-  <div class="content">
-    <h1 class="title">Welcome to IOXIO® Documentation portal</h1>
-    <p class="description">Documentation for various IOXIO® technologies.</p>
-    <p class="description">
-      Visit <A href="https://ioxio.com">IOXIO®</A> to learn more about IOXIO®
-    </p>
-  </div>
-  <img src={DataspaceIcon} alt="Dataspace" />
-</div>
-<div class="cards">
-  {#each cards as card}
-    <div class="card">
-      <h2>{card.title}</h2>
-      <img src={card.img} alt={card.alt} />
-      <a class="link" href={card.href}
-        ><span>Read more </span>
-        <ArrowIcon />
-      </a>
+  <div class="top-section">
+    <div class="content">
+      <h1 class="text">Welcome to IOXIO® Documentation portal</h1>
+      <div class="subtext">
+        <p>Documentation for various IOXIO® technologies.</p>
+        <p>
+          Visit <A href="https://ioxio.com">ioxio.com</A> to learn more about IOXIO®
+        </p>
+      </div>
     </div>
-  {/each}
-  <!-- Reserved -->
-  <div class="reserved" />
+    <img class="hero-image" src={FrontpageHeroImage} alt="Frontpage hero" />
+  </div>
+  <div class="bottom-section">
+    <Grid container>
+      {#each cards as card}
+        <Grid lg={4} sm={12} md={6}>
+          <div class="card">
+            <h2>{card.title}</h2>
+            <img class="card-icon" src={card.img} alt={card.alt} />
+            <a class="link" href={card.href}
+              ><span>Read more </span>
+              <ArrowIcon />
+            </a>
+          </div>
+        </Grid>
+      {/each}
+    </Grid>
+  </div>
 </div>
 
 <style lang="scss">
   @import "$styles/setup";
-  .cards {
+  .wrapper {
     display: flex;
-    flex-direction: row;
-    margin: $spacing-03 0;
+    flex-direction: column;
     gap: $spacing-03;
 
     @include tablet-and-below() {
-      padding: 0 $spacing-02;
-    }
-    @include mobile() {
-      flex-direction: column;
-    }
-  }
-
-  .reserved {
-    flex: 1 0 0;
-    @include tablet-and-below() {
-      display: none;
-    }
-  }
-
-  .card {
-    padding: $spacing-02;
-    display: flex;
-    flex-direction: column;
-    background: #192935;
-    justify-content: space-between;
-    border-radius: 10px;
-    gap: $spacing-01;
-    position: relative;
-    height: 10rem;
-    flex: 1 0 0;
-
-    @include mobile() {
-      flex-basis: auto;
+      padding-top: $spacing-03;
     }
 
-    .link {
+    .top-section {
       display: flex;
-      flex-direction: row;
-      width: max-content;
-      color: $color-success-main;
-      font-weight: 600;
-      line-height: 150%;
-      text-decoration: none;
+      justify-content: space-between;
       align-items: center;
-      gap: $spacing-01;
-      z-index: 99;
-    }
 
-    img {
-      position: absolute;
-      width: 6.125rem;
-      height: 6.125rem;
-      right: $spacing-02;
-      bottom: $spacing-02;
-    }
-
-    h2 {
-      font-size: $h2-font-size;
-      font-weight: 600;
-      line-height: 150%;
-      z-index: 99;
-    }
-  }
-
-  .wrapper {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 $spacing-03;
-
-    @include tablet-and-below() {
-      padding: $spacing-03 $spacing-02;
-    }
-
-    @include mobile() {
-      flex-direction: column;
-    }
-
-    .content {
-      display: flex;
-      flex-direction: column;
-      column-gap: 1rem;
-
-      .title {
-        font-size: $h1-font-size;
-        font-weight: 600;
-        line-height: 150%;
+      @include mobile() {
+        flex-direction: column;
+        gap: $spacing-03;
       }
 
-      .description {
-        font-size: $text-body-font-size;
-        line-height: 150%;
+      .content {
+        .text {
+          font-size: $h1-font-size;
+          font-weight: 600;
+          line-height: 150%;
+          margin-bottom: 1rem;
+        }
+        .subtext {
+          font-size: $text-body-font-size;
+          line-height: 150%;
+          font-weight: 400;
+        }
+      }
+
+      .hero-image {
+        max-width: 42rem;
+        margin: 0 0 0 auto;
+
+        @include tablet() {
+          max-width: 14rem;
+        }
       }
     }
-  }
 
-  img {
-    width: 42.75rem;
-    margin: 0 0 0 auto;
+    .bottom-section {
+      .card {
+        padding: $spacing-02;
+        display: flex;
+        flex-direction: column;
+        background: #192935;
+        justify-content: space-between;
+        border-radius: 10px;
+        gap: $spacing-01;
+        position: relative;
+        height: 10rem;
+
+        .link {
+          display: flex;
+          flex-direction: row;
+          width: max-content;
+          color: $color-success-main;
+          font-weight: 600;
+          line-height: 150%;
+          text-decoration: none;
+          align-items: center;
+          gap: $spacing-01;
+          z-index: 99;
+        }
+
+        h2 {
+          font-size: $h2-font-size;
+          font-weight: 600;
+          line-height: 150%;
+          z-index: 99;
+        }
+
+        .card-icon {
+          position: absolute;
+          width: 6.125rem;
+          height: 6.125rem;
+          right: $spacing-02;
+          bottom: $spacing-02;
+        }
+      }
+    }
   }
 </style>
