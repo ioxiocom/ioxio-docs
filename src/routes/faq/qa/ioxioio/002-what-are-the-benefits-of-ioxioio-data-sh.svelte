6d143ba72040f39126b0d357b9fee97b5b9d34bc
fbjorn <denis@fbjorn.cc>
2025-04-22 13:05:20 +0300

    cleanup

--- src/routes/faq/qa/ioxioio/002-what-are-the-benefits-of-ioxioio-data-sh.svelte
+++ src/routes/faq/qa/ioxioio/002-what-are-the-benefits-of-ioxioio-data-sh.svelte
@@ -8,7 +8,7 @@
   </svelte:fragment>
   
   <svelte:fragment slot="answer">
-    <p>On <a href="http://ioxio.io/">IOXIO.io</a> your company can collaborate with all your data sharing partners. The platform allows you to define what data you want to share, implement data sources, manage access to your data, and consume data from different parties. You keep ownership of your own data,
+    <p>On <a href="https://ioxio.io/">IOXIO.io</a> your company can collaborate with all your data sharing partners. The platform allows you to define what data you want to share, implement data sources, manage access to your data, and consume data from different parties. You keep ownership of your own data,
 while we ensure the data is transferred securely in a harmonized format between the parties. With one integration to <a href="http://ioxio.io/">IOXIO.io</a>, you can share your data with anyone and control access to your data.</p>
 
   </svelte:fragment>
